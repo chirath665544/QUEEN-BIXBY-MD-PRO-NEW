93d988edded93641bf92c77af80901405dc3dcb9
chirath665544 <109127089+chirath665544@users.noreply.github.com>
2022-07-11 23:45:50 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94775969683']
+global.premium = ['94775969683']
+global.ownernomer = '94775969683'
 global.ownername = 'Dark Alpha'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
